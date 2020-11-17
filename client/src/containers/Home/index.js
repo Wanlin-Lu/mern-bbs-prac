0e83a38c4399eaec401cad1bc6b43927b015f01c
Wanlin-Lu <luwanlin@126.com>
2020-11-17 15:53:04 +0800

    Home: userData reStore && redux: auth userdata

--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Route, useRouteMatch, useLocation } from 'react-router-dom'
 import { actions as authActions, getLoggedUser } from '../../redux/modules/auth'
@@ -7,10 +7,30 @@ import Header from '../../components/Header'
 import Post from '../Post'
 import PostList from '../PostList'
 
-const Home = ({ user, logout }) => {
+const Home = ({ user, logout, setLoginInfo }) => {
   const match = useRouteMatch()
   const location = useLocation()
   const username = user && user.username ? user.username : ""
+  let logoutTimer;
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"))
+    if (!user && !user.userId && storedData && storedData.userId) {
+      setLoginInfo(
+        storedData.userId,
+        storedData.username,
+        storedData.email,
+        storedData.token,
+      )
+    }
+
+    if (storedData && storedData.expiration) {
+      const remainingTime = new Date(storedData.expiration).getTime - new Date().getTime()
+      logoutTimer = setTimeout(logout, remainingTime)
+    } else {
+      clearTimeout(logoutTimer)
+    }
+  }, [setLoginInfo, user])
 
   return (
     <div>
