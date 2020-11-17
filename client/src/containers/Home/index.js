d1b9f19efbc74f48973f2a35551e0de5714e8d8b
Wanlin-Lu <luwanlin@126.com>
2020-11-17 15:11:32 +0800

    Home start

--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Route, useRouteMatch, useLocation } from 'react-router-dom'
+import { actions as authActions, getLoggedUser } from '../../redux/modules/auth'
 
-const Home = () => (
-  <h1>Home</h1>
-)
+import Header from '../../components/Header'
+import Post from '../Post'
+import PostList from '../PostList'
 
-export default Home
+const Home = ({ user, logout }) => {
+  const match = useRouteMatch()
+  const location = useLocation()
+  const username = user && user.username ? user.username : ""
+
+  return (
+    <div>
+      <Header
+        username={username}
+        location={location}
+        onLogout={logout}
+      />
+      <Route
+        path={match.url}
+        exact
+        render={props => <PostList {...props} />}
+      />
+      <Route
+        path={`${match.url}/:id`}
+        exact
+        render={props => <Post {...props} />}
+      />
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  user: getLoggedUser(state)
+})
+
+export default connect(mapStateToProps, authActions)(Home)
