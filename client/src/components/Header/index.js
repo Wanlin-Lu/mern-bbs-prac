47c378e2f4b04b879c8da0b47ddd89ad2641b65c
Wanlin-Lu <luwanlin@126.com>
2020-11-17 15:28:08 +0800

    Header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Home = () => (
-  <h1>Header</h1>
+import './style.css'
+
+const Header = ({ username, location, onLogout }) => (
+  <header className="header">
+    <div className="nav">
+      <span className="left-link">
+        <Link to="/">首頁</Link>
+      </span>
+      {username && username.length > 0 ? (
+        <span className="user">
+          當前用戶:{username}&nbsp;
+          <button onClick={onLogout}>注銷</button>
+        </span>
+      ) : (
+          <span className="right-link">
+            <Link to={{ pathname="/auth", state: { from: location } }}>
+              登錄
+            </Link>
+          </span>
+      )}
+    </div>
+  </header>
 )
 
-export default Home
+export default Header
