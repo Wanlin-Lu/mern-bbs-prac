144e7328cebb1f7fbedd09941810e966fce2db3f
Wanlin-Lu <luwanlin@126.com>
2020-11-29 11:16:25 +0800

    CommentsView

--- client/src/containers/Post/components/CommentsView/index.js
+++ client/src/containers/Post/components/CommentsView/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { getFormateDate } from '../../../../utils/date'
+import './style.css'
 
-const CommentsView = () => (
-  <h1>CommentsView</h1>
+const CommentsView = ({ comments }) => (
+  <ul className="commentsView">
+    {comments.map(item => {
+      return (
+        <li key={item.id}>
+          <div>{item.content}</div>
+          <div className="sub">
+            <span>{item.author.username || ""}</span>
+            <span>.</span>
+            <span>{getFormateDate(item.updateAt)}</span>
+          </div>
+        </li>
+      )
+    })}
+  </ul>
 )
 
 export default CommentsView
