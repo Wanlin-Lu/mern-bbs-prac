001413d4366b1de56690b8681952426ac675ba78
Wanlin-Lu <luwanlin@126.com>
2020-11-29 10:52:40 +0800

    PostView

--- client/src/containers/Post/components/PostView/index.js
+++ client/src/containers/Post/components/PostView/index.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import { getFormateDate } from '../../../../utils/date'
+import like from '../../../../images/like.png'
+import './style.css'
 
-const PostView = () => (
-  <h1>PostView</h1>
-)
+const PostView = ({ post, editable, onEditClick }) => {
+  return (
+    <div className="postView">
+      <div>
+        <h2>{post.title}</h2>
+        <div className="mark">
+          <span className="author">{post.author.username}</span>
+          <span>.</span>
+          <span>{getFormateDate(post.updatedAt)}</span>
+          {editable && <span>
+            .<button onClick={onEditClick}>編輯</button>
+          </span>}
+        </div>
+        <div className="content">{post.content}</div>
+      </div>
+      <div className="vote">
+        <span>
+          <img alt="vote" src={like} />
+        </span>
+        <span>{post.vote}</span>
+      </div>
+    </div>
+  )
+}
 
 export default PostView
